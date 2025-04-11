f0df37fc6e20a3a289af8c8632722f3ee32e8ef6
alex-webdeveloper <alexdevnext@gmail.com>
2025-04-11 13:40:11 +0300

    check auth

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -1,8 +1,5 @@
-import { fetchFilteredCustomers } from '@/app/lib/data';
-import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
 
 export const metadata: Metadata = {
   title: 'Customers',
